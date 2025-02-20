25c53e793cb4f88b4f5ab0086bd4c45955c1e4ce
BenHicks00 <bhicks8@my.waketech.edu>
2025-02-20 09:04:37 -0500

    Swapping between scenes added
    Buttons on menus and lose/win screens go to corresponding scenes.

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using static System.TimeZoneInfo;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class LevelLoader : MonoBehaviour
+{
+
+    public Animator transition;
+
+    public float transitionTime = 3f;
+
+    public void LoadHelpMenu()
+    {
+        StartCoroutine(LoadLevel(1));
+    }
+    public void LoadMainMenu()
+    {
+        StartCoroutine(LoadLevel(0));
+    }
+    public void LoadLevelOne()
+    {
+        StartCoroutine(LoadLevel(2));
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+
+
+
+
+    IEnumerator LoadLevel(int levelindex)
+    {
+
+        //transition.SetTrigger("Start");
+
+        yield return new WaitForSeconds(transitionTime);
+
+        SceneManager.LoadScene(levelindex);
+    }
+}
