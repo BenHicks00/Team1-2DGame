3269dd6b9e2d2dccaf3eb30f0aef02ac00b68794
BenHicks00 <bhicks8@my.waketech.edu>
2025-03-10 15:29:36 -0400

    Enemies now flip according to where player is

--- Assets/Scripts/AICHASE.cs
+++ Assets/Scripts/AICHASE.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using static UnityEditor.Searcher.SearcherWindow.Alignment;
+using static UnityEngine.RuleTile.TilingRuleOutput;
 
 public class AICHASE : MonoBehaviour
 {
@@ -10,16 +12,22 @@ public class AICHASE : MonoBehaviour
 
     private float distance;
 
+    private bool isFacingRight = true;
+
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        distance = Vector2.Distance(transform.position,player.transform.position);
+
+
+        distance = Vector2.Distance(transform.position, player.transform.position);
         Vector2 direction = player.transform.position - transform.position;
 
 
@@ -27,6 +35,24 @@ public class AICHASE : MonoBehaviour
         {
             transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
         }
-
+        if (direction.x > 0 && !isFacingRight)
+        {
+            Flip(); 
+            //makes enemy face right
         }
+        else if (direction.x < 0 && isFacingRight)
+        {
+            Flip(); 
+            //make enemy face left
+        }
+        
     }
+
+    private void Flip()
+    {
+        isFacingRight = !isFacingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;  
+        transform.localScale = theScale;
+    }
+}
