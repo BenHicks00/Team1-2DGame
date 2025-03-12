e34911956deb611d5168f38dcf41bb13da63e12e
B-Reel3000 <braedenallaman2@gmail.com>
2025-03-12 13:14:59 -0400

    Tex Attack sounds

--- Assets/Scripts/BasicEnemy.cs
+++ Assets/Scripts/BasicEnemy.cs
@@ -8,15 +8,22 @@ public class BasicEnemy : MonoBehaviour
     public ParticleSystem redParticles;
     public AudioSource audiosource;
     public AudioClip deathSFX;
-    public AudioClip damageSFX; // Sound effect when hit
+    public AudioClip damageSFX;
+    public AudioClip bossFightMusic; // Music to play during animation
     private Rigidbody2D rb;
+    private Animator animator;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>(); // Get the Animator
+    }
 
     public void TakeDamage(float damage)
     {
         health -= damage;
         redParticles.Play();
 
-        // Play damage sound effect when hit
         if (audiosource != null && damageSFX != null)
         {
             audiosource.PlayOneShot(damageSFX);
@@ -30,17 +37,12 @@ public class BasicEnemy : MonoBehaviour
         }
     }
 
-    private void Start()
-    {
-        rb = GetComponent<Rigidbody2D>();
-    }
-
     private void FixedUpdate()
     {
         // rb.velocity = new Vector3(speed, rb.velocity.y, 0);
     }
 
-    private void OnCollisionEnter2D(Collision2D collision) // Using Collision instead of Trigger
+    private void OnCollisionEnter2D(Collision2D collision)
     {
         Debug.Log($"Enemy collided with: {collision.gameObject.name}");
 
@@ -57,6 +59,29 @@ public class BasicEnemy : MonoBehaviour
             else
             {
                 Debug.LogWarning("PlayerHealth script not found on player!");
+            }
+        }
+    }
+
+    // NEW: Play continuous audio when the "Tex Boss fight1" animation is triggered
+    private void Update()
+    {
+        if (animator.GetCurrentAnimatorStateInfo(0).IsName("Tex Boss fight1"))
+        {
+            if (!audiosource.isPlaying) // Prevent restarting the audio each frame
+            {
+                audiosource.clip = bossFightMusic;
+                audiosource.loop = true; // Ensure the sound loops
+                audiosource.Play();
+                Debug.Log("Boss fight music started!");
+            }
+        }
+        else
+        {
+            if (audiosource.clip == bossFightMusic) // Stop only if it's playing the boss music
+            {
+                audiosource.Stop();
+                Debug.Log("Boss fight music stopped!");
             }
         }
     }
